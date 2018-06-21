da9dc765b3961ef16370e942bd38b4f85d78e033
Tyler Hughes <tyler@tylerhughes.info>
2018-06-21 08:20:46 -0500

    Added several files to the Ui project

---
+++ Telegraph.UI/wwwroot/js/Telegraph.jsx
@@ -0,0 +1,15 @@
+﻿class TextField extends React.Component {
+  render() {
+    return (
+      <div className="textField">
+        Hello, world! I am a textfield.
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <TextField />,
+  document.getElementById('content')
+);
+
