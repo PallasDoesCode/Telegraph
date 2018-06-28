0d06ac8192c6b15cacb2bc4cf251f56b211fe32f
Tyler Hughes <tylerbhughes@users.noreply.github.com>
2018-06-28 16:14:18 -0500

    Created appveyor yaml script (#1)

---
+++ appveyor.ps1
@@ -0,0 +1,70 @@
+if($env:APPVEYOR_REPO_BRANCH -eq "release"){
+	[string]$nugetApiKey = ($env:NuGetApiKey)
+
+    PublishNuGetPackage -nugetPackage Telegraph -nugetApiKey $nugetApiKey
+} 
+else {
+    # Untested commit so don't publish to Nuget
+}
+
+
+function Get-Nuget{
+    [cmdletbinding()]
+    param(
+        $toolsDir = ("$env:LOCALAPPDATA\Telegraph\tools\"),
+        $nugetDownloadUrl = 'http://nuget.org/nuget.exe'
+    )
+    process{
+        $nugetDestPath = Join-Path -Path $toolsDir -ChildPath nuget.exe
+
+        if(!(Test-Path $nugetDestPath)){
+            $nugetDir = ([System.IO.Path]::GetDirectoryName($nugetDestPath))
+            if(!(Test-Path $nugetDir)){
+                New-Item -Path $nugetDir -ItemType Directory | Out-Null
+            }
+
+            'Downloading nuget.exe' | Write-Message
+            (New-Object System.Net.WebClient).DownloadFile($nugetDownloadUrl, $nugetDestPath)
+
+            # double check that is was written to disk
+            if(!(Test-Path $nugetDestPath)){
+                throw 'Unable to download nuget.'
+            }
+        }
+
+        # return the path of the file
+        $nugetDestPath
+    }
+}
+
+
+function PublishNuGetPackage{
+    [cmdletbinding(SupportsShouldProcess=$true)]
+    param(
+        [Parameter(Mandatory=$true,ValueFromPipeline=$true)]
+        [string[]]$nugetPackage,
+
+        [Parameter(Mandatory=$true)]
+        $nugetApiKey,
+
+        [Parameter()]
+        $source = 'https://www.nuget.org/api/v2/package'
+    )
+    process{
+        foreach($pkg in $nugetPackage){
+            $pkgPath = (get-item $pkg).FullName
+            $cmdArgs = @('push',$pkgPath,$nugetApiKey,'-source',$source,'-NonInteractive')
+			$logfilepath = "$([System.IO.Path]::GetTempFileName()).log"
+
+            'Publishing nuget package [{0}]' -f $pkgPath | Write-Message
+
+            $filteredCmd = Filter-String ('Publishing nuget package with the following args: [nuget.exe {0}]' -f ($cmdArgs -join ' '))
+            if($PSCmdlet.ShouldProcess($env:COMPUTERNAME, $filteredCmd)){
+                &(Get-Nuget) $cmdArgs *> $logfilepath
+                if($LASTEXITCODE -ne 0){
+                    throw ('nuget.exe failed with the following error code [{0}]' -f $LASTEXITCODE)
+                }
+            }
+        }
+    }
+}
